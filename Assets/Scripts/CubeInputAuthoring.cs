6191d653cb700f990ae569bf08cd6e7bd36e672d
VirrorE <vieqqedf@gmail.com>
2023-07-11 23:53:16 +0800

    <feature> movement sync

---
+++ Assets/Scripts/CubeInputAuthoring.cs
@@ -0,0 +1,44 @@
+using Unity.Entities;
+using Unity.NetCode;
+using UnityEngine;
+
+[GhostComponent(PrefabType=GhostPrefabType.AllPredicted)]
+public struct CubeInput : IInputComponentData {
+    public int Horizontal;
+    public int Vertical;
+}
+
+[DisallowMultipleComponent]
+public class CubeInputAuthoring : MonoBehaviour {
+    class Baking : Unity.Entities.Baker<CubeInputAuthoring> {
+        public override void Bake(CubeInputAuthoring authoring) {
+            AddComponent<CubeInput>();
+        }
+    }
+}
+
+[UpdateInGroup(typeof(GhostInputSystemGroup))]
+public partial struct SampleCubeInput : ISystem {
+    public void OnUpdate(ref SystemState state) {
+        bool left = UnityEngine.Input.GetKey("left");
+        bool right = UnityEngine.Input.GetKey("right");
+        bool down = UnityEngine.Input.GetKey("down");
+        bool up = UnityEngine.Input.GetKey("up");
+
+        foreach (var playerInput in SystemAPI.Query<RefRW<CubeInput>>().WithAll<GhostOwnerIsLocal>()) {
+            playerInput.ValueRW = default;
+            if (left) {
+                playerInput.ValueRW.Horizontal -= 1;
+            }
+            if (right) {
+                playerInput.ValueRW.Horizontal += 1;
+            }
+            if (down) {
+                playerInput.ValueRW.Vertical -= 1;
+            }
+            if (up) {
+                playerInput.ValueRW.Vertical += 1;
+            }
+        }
+    }
+}
