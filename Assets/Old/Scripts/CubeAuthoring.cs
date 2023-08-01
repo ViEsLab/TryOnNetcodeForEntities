8e7111890c6d655cc58514e1f7c640d9812476b9
叶凌峰 <vieqqedf@gmail.com>
2023-08-01 17:49:21 +0800

    <misc> comment

--- Assets/Old/Scripts/CubeAuthoring.cs
+++ Assets/Old/Scripts/CubeAuthoring.cs
@@ -1,18 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.ComponentModel;
-using Unity.Entities;
-using UnityEngine;
-
-public struct TestCube : IComponentData {
-}
-
-[DisallowMultipleComponent]
-public class CubeAuthoring : MonoBehaviour {
-    class Baker : Baker<CubeAuthoring> {
-        public override void Bake(CubeAuthoring authoring) {
-            TestCube component = default(TestCube);
-            AddComponent(component);
-        }
-    }
-}
+// using System.Collections;
+// using System.Collections.Generic;
+// using System.ComponentModel;
+// using Unity.Entities;
+// using UnityEngine;
+//
+// public struct TestCube : IComponentData {
+// }
+//
+// [DisallowMultipleComponent]
+// public class CubeAuthoring : MonoBehaviour {
+//     class Baker : Baker<CubeAuthoring> {
+//         public override void Bake(CubeAuthoring authoring) {
+//             TestCube component = default(TestCube);
+//             AddComponent(component);
+//         }
+//     }
+// }
